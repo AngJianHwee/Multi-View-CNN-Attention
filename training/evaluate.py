cc6ca4e88b91d4699ed22ee0d64b6bf6849e4aa9
AngJianHwee <megahwee@gmail.com>
2025-04-01 03:03:52 +0000

    [Debug] Add temporary breaks in evaluation and training loops

--- training/evaluate.py
+++ training/evaluate.py
@@ -12,9 +12,9 @@ def evaluate(model, device, loader):
             fx, _ = model(x, x, x)  # Ignore features during evaluation
             epoch_acc += (fx.argmax(1) == y).sum().item()
             total_samples += y.size(0)
-            # break   # TODO: Remove this break to evaluate on the entire dataset
+            break   # TODO: Remove this break to evaluate on the entire dataset
             
-            # if i > len(loader) // 10: # TODO: Remove this break to train on the entire dataset
-            #     # Only break after 10% of the dataset
-            #     break
+            if i > len(loader) // 10: # TODO: Remove this break to train on the entire dataset
+                # Only break after 10% of the dataset
+                break
     return epoch_acc / total_samples  # Normalize by total samples instead of dataset length
